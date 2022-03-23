705b3fbfb860ff0bffa41ed2ecf0d56f05721ee6
leonardo ferreira <leonardo.ferreira@objective.com.br>
2022-03-23 17:29:18 -0300

    styles card loader

--- frontweb/src/pages/Movies/CardLoader/index.tsx
+++ frontweb/src/pages/Movies/CardLoader/index.tsx
@@ -1,16 +1,17 @@
 import ContentLoader from 'react-content-loader';
+import './styles.css';
 
 const CardLoader = () => (
   <div className="card-loader-container">
     <ContentLoader
-      speed={2}
-      width={320}
-      height={460}
-      viewBox="0 0 320 460"
-      backgroundColor="#ecebeb"
-      foregroundColor="#d6d2d2"
+      speed={1}
+      width={300}
+      height={400}
+      viewBox="0 0 300 400"
+      backgroundColor="#525252"
+      foregroundColor="#6C6C6C"
     >
-      <rect x="0" y="0" rx="2" ry="2" width="300" height="300" />
+      <rect x="0" y="0" rx="2" ry="2" width="300" height="400" />
     </ContentLoader>
   </div>
 );
