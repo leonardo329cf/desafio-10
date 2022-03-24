e87107c5ef1225f0ea0f80532e390ba62f80b399
leonardo ferreira <leonardo.ferreira@objective.com.br>
2022-03-24 14:28:25 -0300

    Add Tast to app

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Routes from 'Routes';
 import { useState } from 'react';
 import { AuthContext, AuthContextData } from 'AuthContext';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const [authContextData, setAuthContextData] = useState<AuthContextData>({
@@ -12,6 +13,7 @@ function App() {
   return (
     <AuthContext.Provider value={{ authContextData, setAuthContextData }}>
       <Routes />
+      <ToastContainer />
     </AuthContext.Provider>
   );
 }
